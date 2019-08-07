a03354d355ed6f59805f4d49cbf3b2f698085d47
Andromander <cathy@feodoroff.com>
2019-08-07 21:05:29 +1000

    Nifty 1.4.1
    1.4 will work between 1.13.2 and 1.14.4 early versions. 1.4.1 is exclusive to Forge 28.0.45 and up

--- src/main/java/com/androsa/cml/CMLConfig.java
+++ src/main/java/com/androsa/cml/CMLConfig.java
@@ -1,62 +1,72 @@
 package com.androsa.cml;
 
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import com.google.common.collect.Lists;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Config(modid = CascadingMemeMod.MODID)
+import java.util.List;
+
+import static net.minecraftforge.common.ForgeConfigSpec.*;
+
 @Mod.EventBusSubscriber(modid = CascadingMemeMod.MODID)
 public class CMLConfig {
     private static final String config = CascadingMemeMod.MODID + ".config.";
 
-    @Config.LangKey(config + "pool_size")
-    @Config.RequiresMcRestart
-    @Config.RangeInt(min = 0)
-    @Config.Comment("Adjust the sensitivity of the Cascading Meme Checker. Higher values increase the sensitivity.")
-    public static int poolSize = 99;
+    public static IntValue poolSize;
+    public static IntValue threshold;
+    public static IntValue timeToCheck;
+    public static ConfigValue<List<? extends String>> highLatentMeme;
+    public static BooleanValue tooManyModToggle;
+    public static IntValue maximumModCount;
+    public static BooleanValue tooManyToCount;
+    public static IntValue maximumMemeThreshold;
 
-    @Config.LangKey(config + "threshold")
-    @Config.RequiresMcRestart
-    @Config.Comment("Adjust the Cascading Meme Threshold. Lower values increase the sensitivity. NOTE: If the Threshold is too high, the Cascading Meme Checker cannot function properly. Fallbacks are in place in the event the Threshold is too high.")
-    public static int threshold = 90;
+    public CMLConfig(Builder builder) {
+        poolSize = builder
+                        .translation(config + "pool_size")
+                        .worldRestart()
+                        .comment("Adjust the sensitivity of the Cascading Meme Checker. Higher values increase the sensitivity.")
+                        .defineInRange("poolSize", 99, 0, Integer.MAX_VALUE);
 
-    @Config.LangKey(config + "time_to_check")
-    @Config.RequiresMcRestart
-    @Config.RangeInt(min = 0)
-    @Config.Comment("Adjust the frequency of the checker. The frequency is per tick, so every 20 ticks is 1 second. Lower values increase the frequency the checker will scan for memes.")
-    public static int timeToCheck = 100;
+        threshold = builder
+                        .translation(config + "threshold")
+                        .worldRestart()
+                        .comment("Adjust the Cascading Meme Threshold. Lower values increase the sensitivity. NOTE: If the Threshold is too high, the Cascading Meme Checker cannot function properly. Fallbacks are in place in the event the Threshold is too high.")
+                        .defineInRange("threshold", 90, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
-    @Config.LangKey(config + "meme_latent_mods")
-    @Config.RequiresMcRestart
-    @Config.Comment("If you suspect a mod is leaking too many memes, but the checker is unable to pick them up, you can list what mods increase the chances of picking up memes caused by the mod. Mods must be listed by their Mod ID and separated by a comma.")
-    public static String[] highLatentMeme = { "examplemod" };
+        timeToCheck = builder
+                        .translation(config + "time_to_check")
+                        .worldRestart()
+                        .comment("Adjust the frequency of the checker. The frequency is per tick, so every 20 ticks is 1 second. Lower values increase the frequency the checker will scan for memes.")
+                        .defineInRange("timeToCheck", 100, 0, Integer.MAX_VALUE);
 
-    @Config.LangKey(config + "too_many_mod_toggle")
-    @Config.RequiresMcRestart
-    @Config.Comment("At a certain mod count threshold, Cascading Meme Lag will terminate the attempted session due to too many mods. This is to prevent an overload of Cascading Memes onset by an overabundance of mods. Turning this off will mean that no amount of mods will terminate the session.")
-    public static boolean tooManyModToggle = false;
+        highLatentMeme = builder
+                        .translation(config + "meme_latent_mods")
+                        .worldRestart()
+                        .comment("If you suspect a mod is leaking too many memes, but the checker is unable to pick them up, you can list what mods increase the chances of picking up memes caused by the mod. Mods must be listed by their Mod ID and separated by a comma.")
+                        .defineList("highLatentMeme", Lists.newArrayList("examplemod"), o -> o instanceof String);
 
-    @Config.LangKey(config + "maximum_mod_count")
-    @Config.RequiresMcRestart
-    @Config.Comment("The maximum mod count until Cascading Meme Lag will terminate session due to an overabundance of mods. This option does nothing if Crash On Too Many Mods is disabled.")
-    public static int maximumModCount = 350;
+        tooManyModToggle = builder
+                        .translation(config + "too_many_mod_toggle")
+                        .worldRestart()
+                        .comment("At a certain mod count threshold, Cascading Meme Lag will terminate the attempted session due to too many mods. This is to prevent an overload of Cascading Memes onset by an overabundance of mods. Turning this off will mean that no amount of mods will terminate the session.")
+                        .define("tooManyModToggle", false);
 
-    @Config.LangKey(config + "too_high_count_toggle")
-    @Config.RequiresMcRestart
-    @Config.Comment("If the resulting Meme Threshold is above the set limit, Cascading Meme Lag will terminate the session due to an exceeded threshold. This is to prevent an overload of Cascading Memes onset by an excess abundance of memes. Turning this off will mean that no amount of threshold will terminate the session.")
-    public static boolean tooManyToCount = false;
+        maximumModCount = builder
+                        .translation(config + "maximum_mod_count")
+                        .worldRestart()
+                        .comment("The maximum mod count until Cascading Meme Lag will terminate session due to an overabundance of mods. This option does nothing if Crash On Too Many Mods is disabled.")
+                        .defineInRange("maximumModCount", 350, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
-    @Config.LangKey(config + "maximum_meme_count")
-    @Config.RequiresMcRestart
-    @Config.Comment("The maximum meme threshold until Cascading Meme Lag will terminate session due to an overabundance of memes. This option does nothing if Crash On Too Many Memes is disabled.")
-    public static int maximumMemeThreshold = 700;
+        tooManyToCount = builder
+                        .translation(config + "too_high_count_toggle")
+                        .worldRestart()
+                        .comment("If the resulting Meme Threshold is above the set limit, Cascading Meme Lag will terminate the session due to an exceeded threshold. This is to prevent an overload of Cascading Memes onset by an excess abundance of memes. Turning this off will mean that no amount of threshold will terminate the session.")
+                        .define("tooManyToCount", false);
 
-    @SubscribeEvent
-    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
-        if (event.getModID().equals(CascadingMemeMod.MODID)) {
-            ConfigManager.sync(CascadingMemeMod.MODID, Config.Type.INSTANCE);
-        }
+        maximumMemeThreshold = builder
+                        .translation(config + "maximum_meme_count")
+                        .worldRestart()
+                        .comment("The maximum meme threshold until Cascading Meme Lag will terminate session due to an overabundance of memes. This option does nothing if Crash On Too Many Memes is disabled.")
+                        .defineInRange("maximumMemeThreshold", 700, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
--- src/main/java/com/androsa/cml/CascadingMemeMod.java
+++ src/main/java/com/androsa/cml/CascadingMemeMod.java
@@ -2,13 +2,19 @@ package com.androsa.cml;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Loader;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,59 +22,66 @@ import java.util.Random;
 
 import static com.androsa.cml.CMLConfig.*;
 
-@Mod(modid = CascadingMemeMod.MODID, name = CascadingMemeMod.NAME, version = CascadingMemeMod.VERSION, clientSideOnly = true)
+@Mod(CascadingMemeMod.MODID)
 public class CascadingMemeMod {
     public static final String MODID = "cml";
-    public static final String NAME = "Cascading Meme Lag";
-    public static final String VERSION = "1.2";
     private static final Logger logger = LogManager.getLogger(MODID);
+    public static CMLConfig config;
+
     private static int modCount;
     private static int minimumChance;
     private static int tickTime = 0;
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        //Count out the number of mods, minus the necessities (Minecraft, Forge, FML, MCP)
-        int modSize = Loader.instance().getIndexedModList().size() - 4;
+    public CascadingMemeMod() {
+        final Pair<CMLConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(CMLConfig::new);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, specPair.getRight());
+        config = specPair.getLeft();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void init(FMLClientSetupEvent event) {
+        //Count out the number of mods, minus the necessities
+        int modSize = ModList.get().getMods().size() - 2;
         logger.debug("We have found " + modSize + " mods to count.");
-        if (tooManyModToggle && modSize > maximumModCount) {
-            logger.fatal("CAUTION: TOO MANY MODS HAVE BEEN DETECTED. {} HAVE BEEN FOUND, BUT ONLY {} ARE ALLOWED.", modSize, maximumModCount);
+        if (tooManyModToggle.get() && modSize > maximumModCount.get()) {
+            logger.fatal("CAUTION: TOO MANY MODS HAVE BEEN DETECTED. {} HAVE BEEN FOUND, BUT ONLY {} ARE ALLOWED.", modSize, maximumModCount.get());
             throw new CascadingMemeException("There are too many mods loaded. Cascading Meme Lag has terminated the session. Adjust the number of allowed mods or disable this feature in the configuration file.");
         }
         //Check the config for modids and see if those mods are loaded. Add 5 for each modid found and loaded
-        for (String modid : highLatentMeme) {
-            if (Loader.isModLoaded(modid)) {
+        for (String modid : highLatentMeme.get()) {
+            if (ModList.get().isLoaded(modid)) {
                 logger.debug("Detected a high meme-latent mod, {}.", modid);
                 modCount += 5;
             }
         }
         //Looking at you, Twilight Forest...
-        if (Loader.isModLoaded("twilightforest")) {
+        if (ModList.get().isLoaded("twilightforest")) {
             modSize *= 2;
             logger.info("Twilight Forest has been identified in the list of mods available. Meme rates are doubled.");
         }
 
-        modCount = modSize + poolSize;
+        modCount = modSize + poolSize.get();
         logger.debug("The size of the randomizer is {}.", modCount);
-        if (tooManyToCount && modCount > maximumMemeThreshold) {
-            logger.fatal("CAUTION: OVERABUNDANCE OF MEMES WILL OVERLOAD THE SESSION. {} HAVE BEEN FOUND, BUT THE THRESHOLD IS AT {}.", modCount, maximumMemeThreshold);
+        if (tooManyToCount.get() && modCount > maximumMemeThreshold.get()) {
+            logger.fatal("CAUTION: OVERABUNDANCE OF MEMES WILL OVERLOAD THE SESSION. {} HAVE BEEN FOUND, BUT THE THRESHOLD IS AT {}.", modCount, maximumMemeThreshold.get());
             throw new CascadingMemeException("The size of the randomizer exceeds the recommended threshold. Cascading Meme Lag has terminated the session. Adjust the maximum threshold or disable this feature in the configuration file.");
         }
         //If by some chance the randomizer will never yield a result, say that the random number will be below the threshold, make our own. The chance will now be 10 below the total
         //Otherwise, just use the exact value.
-        if (threshold >= modCount) {
+        if (threshold.get() >= modCount) {
             minimumChance = modCount - 10;
             logger.error("An error has occurred. Cascading Meme Lag's checker cannot work with the current Meme Threshold. Automatically adjusting the Meme Threshold...");
         } else {
-            minimumChance = threshold;
+            minimumChance = threshold.get();
         }
         logger.debug("Meme Threshold has been set to {}.", minimumChance);
     }
 
-    @Mod.EventBusSubscriber(value = Side.CLIENT)
+    @Mod.EventBusSubscriber(value = Dist.CLIENT)
     public static class ClientEvents {
         private static final Random random = new Random();
-        private static int randFrame = modCount;
         private static String errorLine = "Minecraft loaded a new chunk [{},{}] in dimension {} ({}) while populating chunk [{},{}], causing cascading meme lag.";
 
         private static int getChunkCoordinate(int max) {
@@ -78,22 +91,22 @@ public class CascadingMemeMod {
         @SubscribeEvent
         public static void onClientTick(TickEvent.ClientTickEvent e) {
             if (e.phase != TickEvent.Phase.END) return;
-            World world = Minecraft.getMinecraft().world;
+            World world = Minecraft.getInstance().world;
 
             if (world != null) {
                 if (tickTime == 0) {
-                    if (random.nextInt(randFrame) > minimumChance) {
-                        logger.warn(errorLine, getChunkCoordinate(187500), getChunkCoordinate(187500), world.provider.getDimension(), world.provider.getDimensionType().getName(), getChunkCoordinate(187500), getChunkCoordinate(187500));
+                    if (random.nextInt(modCount) > minimumChance) {
+                        logger.warn(errorLine, getChunkCoordinate(187500), getChunkCoordinate(187500), world.dimension.getType().getId(), DimensionType.getKey(world.dimension.getType()), getChunkCoordinate(187500), getChunkCoordinate(187500));
                         logger.warn(randomMessage());
                     }
-                    tickTime = timeToCheck;
+                    tickTime = timeToCheck.get();
                 }
                 --tickTime;
             }
         }
 
         static String randomMessage() {
-            int randomSize = Loader.isModLoaded("twilightforest") ? 7 : 6;
+            int randomSize = ModList.get().isLoaded("twilightforest") ? 7 : 6;
 
             switch (random.nextInt(randomSize)) {
                 case 0:
